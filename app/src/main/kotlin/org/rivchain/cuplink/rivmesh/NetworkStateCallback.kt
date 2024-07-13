a742f212c2789a593dfbe283d9e10cb8fb8fbb96
Vadym Vikulin <vadym.vikulin@rivchain.org>
2024-07-13 14:20:21 +0300

    RCL-73 Re-connect peers once networking is available

--- app/src/main/kotlin/org/rivchain/cuplink/rivmesh/NetworkStateCallback.kt
+++ app/src/main/kotlin/org/rivchain/cuplink/rivmesh/NetworkStateCallback.kt
@@ -1,42 +1,41 @@
 package org.rivchain.cuplink.rivmesh
 
 import android.content.Context
+import android.content.Intent
 import android.net.ConnectivityManager
 import android.net.Network
 import android.net.NetworkCapabilities
 import android.net.NetworkRequest
-import android.util.Log
-
-
-private const val TAG = "Network"
+import android.os.Build
+import org.rivchain.cuplink.MainService
+import org.rivchain.cuplink.util.Log
 
 class NetworkStateCallback(val context: Context) : ConnectivityManager.NetworkCallback() {
 
     override fun onAvailable(network: Network) {
         super.onAvailable(network)
-        Log.d(TAG, "onAvailable")
-        /*
-        val preferences = PreferenceManager.getDefaultSharedPreferences(context)
-        if (preferences.getBoolean(PREF_KEY_ENABLED, true)) {
-            Thread {
-                // The message often arrives before the connection is fully established
-                Thread.sleep(1000)
-                val intent = Intent(context, MainService::class.java)
-                intent.action = MainService.ACTION_CONNECT
-                try {
+        Log.d(this, "onAvailable")
+
+        Thread {
+            // The message often arrives before the connection is fully established
+            Thread.sleep(1000)
+            val intent = Intent(context, MainService::class.java)
+            intent.action = MainService.ACTION_CONNECT
+            try {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    context.startForegroundService(intent)
+                } else {
                     context.startService(intent)
-                } catch (e: IllegalStateException) {
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                        context.startForegroundService(intent)
-                    }
                 }
-            }.start()
-        }*/
+            } catch (e: IllegalStateException) {
+                e.printStackTrace()
+            }
+        }.start()
     }
 
     override fun onLost(network: Network) {
         super.onLost(network)
-        Log.d(TAG, "onLost")
+        Log.d(this, "onLost")
     }
 
     fun register() {
