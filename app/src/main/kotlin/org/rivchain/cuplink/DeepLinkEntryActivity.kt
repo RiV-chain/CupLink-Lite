9a943d28db8b9881904cd2e87745a45a651a82c7
Vadym Vikulin <vadym.vikulin@rivchain.org>
2024-11-04 22:36:40 +0200

    RCL-175 Add empty activity behind deeplink dialog

--- app/src/main/kotlin/org/rivchain/cuplink/DeepLinkEntryActivity.kt
+++ app/src/main/kotlin/org/rivchain/cuplink/DeepLinkEntryActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 class DeepLinkEntryActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_empty)
         val intent: Intent = MainActivity.clearTop(this)
         val data = getIntent().data
         intent.setData(data)
