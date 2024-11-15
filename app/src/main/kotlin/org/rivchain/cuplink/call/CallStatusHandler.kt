f416567dc423c1e3249f2591a8d5c7e62f66bd9c
Vadym Vikulin <vadym.vikulin@rivchain.org>
2024-11-16 00:03:51 +0200

    RCL-187 In Call Page add On Hold feature

---
+++ app/src/main/kotlin/org/rivchain/cuplink/call/CallStatusHandler.kt
@@ -0,0 +1,112 @@
+package org.rivchain.cuplink.call
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import android.telephony.PhoneStateListener
+import android.telephony.TelephonyCallback
+import android.telephony.TelephonyManager
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import org.json.JSONObject
+import org.rivchain.cuplink.message.ActionMessageDispatcher
+import org.rivchain.cuplink.util.ServiceUtil
+
+class CallStatusHandler(private val context: Context, private val dispatcher: ActionMessageDispatcher) {
+
+    private var telephonyCallback: TelephonyCallback? = null
+    private var callStatusListener: PhoneStateListener? = null
+    private lateinit var telephonyManager: TelephonyManager
+    private var onHold = false
+
+    fun startCallStatusListening() {
+
+        if(ActivityCompat.checkSelfPermission(this.context, Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED) {
+
+            telephonyManager = ServiceUtil.getTelephonyManager(context)
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                telephonyCallback =
+                    object : TelephonyCallback(), TelephonyCallback.CallStateListener {
+
+                        override fun onCallStateChanged(state: Int) {
+                            processCallStateChanged(state)
+                        }
+                    }
+                telephonyManager.registerTelephonyCallback(
+                    context.mainExecutor,
+                    telephonyCallback!!
+                )
+
+            } else {
+                callStatusListener = object : PhoneStateListener() {
+
+                    override fun onCallStateChanged(state: Int, phoneNumber: String?) {
+                        super.onCallStateChanged(state, phoneNumber)
+                        processCallStateChanged(state)
+                    }
+                }
+                callStatusListener?.let {
+                    telephonyManager.listen(it, PhoneStateListener.LISTEN_CALL_STATE)
+                }
+            }
+        }
+    }
+
+    private fun processCallStateChanged(state: Int){
+        when (state) {
+            TelephonyManager.CALL_STATE_RINGING -> {
+                onHold = true
+                Log.d("CallStatusHandler", "Incoming GSM call detected")
+                Log.d("VoIP", "Muting VoIP call")
+                val obj = JSONObject().apply {
+                    put("action", "on_hold")
+                }
+                Thread {
+                    dispatcher.sendMessage(obj)
+                    dispatcher.closeSocket()
+                }.start()
+            }
+            TelephonyManager.CALL_STATE_OFFHOOK -> {
+                onHold = true
+                Log.d("CallStatusHandler", "GSM call in progress")
+                val obj = JSONObject().apply {
+                    put("action", "on_hold")
+                }
+                Thread {
+                    dispatcher.sendMessage(obj)
+                    dispatcher.closeSocket()
+                }.start()
+
+            }
+            TelephonyManager.CALL_STATE_IDLE -> {
+                Log.d("CallStatusHandler", "No active GSM call")
+                if (onHold) {
+                    onHold = false
+                    val obj = JSONObject().apply {
+                        put("action", "resume")
+                    }
+                    Thread {
+                        dispatcher.sendMessage(obj)
+                        dispatcher.closeSocket()
+                    }.start()
+                }
+            }
+        }
+    }
+
+
+    fun stopCallStatusListening() {
+        callStatusListener?.let {
+            telephonyManager.listen(it, PhoneStateListener.LISTEN_NONE)
+        }
+        callStatusListener = null
+        telephonyCallback?.let {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                telephonyManager.unregisterTelephonyCallback(telephonyCallback!!)
+            }
+        }
+        telephonyCallback = null
+    }
+}
--- app/src/main/kotlin/org/rivchain/cuplink/call/RTCCall.kt
+++ app/src/main/kotlin/org/rivchain/cuplink/call/RTCCall.kt
@@ -7,9 +7,9 @@ import android.os.Handler
 import android.os.Looper
 import org.json.JSONException
 import org.json.JSONObject
-import org.rivchain.cuplink.Crypto
 import org.rivchain.cuplink.DatabaseCache
 import org.rivchain.cuplink.MainService
+import org.rivchain.cuplink.message.ActionMessageDispatcher
 import org.rivchain.cuplink.model.Contact
 import org.rivchain.cuplink.util.Log
 import org.rivchain.cuplink.util.Utils
@@ -56,6 +56,7 @@ import java.net.Socket
 import java.nio.ByteBuffer
 import java.util.Timer
 import java.util.TimerTask
+
 
 class RTCCall : RTCPeerConnection {
     private lateinit var factory: PeerConnectionFactory
@@ -262,7 +263,7 @@ class RTCCall : RTCPeerConnection {
         service: MainService,
         contact: Contact,
         commSocket: Socket,
-        offer: String
+        offer: String,
     ) : super(service, contact, commSocket) {
         Log.d(this, "RTCCall() created for incoming calls")
 
@@ -276,15 +277,16 @@ class RTCCall : RTCPeerConnection {
             addAction(Intent.ACTION_USER_PRESENT)
         }
         service.registerReceiver(screenStateReceiver, filter)
+
     }
 
     // called for outgoing calls
     constructor(
         service: MainService,
-        contact: Contact
+        contact: Contact,
     ) : super(service, contact, null) {
         Log.d(this, "RTCCall() created for outgoing calls")
-
+        outgoingRTCCall = this
         createMediaConstraints()
     }
 
@@ -536,7 +538,7 @@ class RTCCall : RTCPeerConnection {
             }
 
             override fun onWebRtcAudioRecordStartError(
-                errorCode: JavaAudioDeviceModule.AudioRecordStartErrorCode, errorMessage: String
+                errorCode: JavaAudioDeviceModule.AudioRecordStartErrorCode, errorMessage: String,
             ) {
                 Log.e(this, "onWebRtcAudioRecordStartError: $errorCode. $errorMessage")
                 callActivity!!.showTextMessage(errorMessage)
@@ -554,7 +556,7 @@ class RTCCall : RTCPeerConnection {
             }
 
             override fun onWebRtcAudioTrackStartError(
-                errorCode: JavaAudioDeviceModule.AudioTrackStartErrorCode, errorMessage: String
+                errorCode: JavaAudioDeviceModule.AudioTrackStartErrorCode, errorMessage: String,
             ) {
                 Log.e(this, "onWebRtcAudioTrackStartError: $errorCode. $errorMessage")
                 callActivity!!.showTextMessage(errorMessage)
@@ -707,7 +709,6 @@ class RTCCall : RTCPeerConnection {
         execute {
             Log.d(this, "initIncoming() executor start")
             val settings = DatabaseCache.database.settings
-            val remoteAddress = commSocket!!.remoteSocketAddress as InetSocketAddress
             val rtcConfig = RTCConfiguration(emptyList())
             rtcConfig.sdpSemantics = SdpSemantics.UNIFIED_PLAN
             rtcConfig.continualGatheringPolicy = ContinualGatheringPolicy.GATHER_ONCE
@@ -745,28 +746,7 @@ class RTCCall : RTCPeerConnection {
 
                     if (iceGatheringState == IceGatheringState.COMPLETE) {
                         try {
-                            val ownPublicKey = settings.publicKey
-                            val ownSecretKey = settings.secretKey
-                            val pw = PacketWriter(commSocket!!)
-                            val obj = JSONObject()
-                            obj.put("action", "connected")
-                            obj.put("answer", peerConnection!!.localDescription.description)
-                            val encrypted = Crypto.encryptMessage(
-                                obj.toString(),
-                                contact.publicKey,
-                                ownPublicKey,
-                                ownSecretKey
-                            )
-                            if (encrypted != null) {
-                                Log.d(this, "onIceGatheringChange() send connected")
-                                pw.writeMessage(encrypted)
-                                callActivity?.onRemoteAddressChange(remoteAddress, true)
-                                // connected state will be reported by WebRTC onIceConnectionChange()
-                                //reportStateChange(CallState.CONNECTED)
-                            } else {
-                                Log.d(this, "onIceGatheringChange() encryption failed")
-                                reportStateChange(CallState.ERROR_COMMUNICATION)
-                            }
+                            ActionMessageDispatcher(contact, this@RTCCall, commSocket!!).answerCall(peerConnection!!.localDescription.description)
                         } catch (e: Exception) {
                             e.printStackTrace()
                             reportStateChange(CallState.ERROR_COMMUNICATION)
