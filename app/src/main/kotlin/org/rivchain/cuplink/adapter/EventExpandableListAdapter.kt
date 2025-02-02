a7937e377eac8c8abb0f5875856fd6487f02bc64
Vadym Vikulin <vadym.vikulin@rivchain.org>
2025-02-02 21:42:35 +0200

    RCL-320 Update group list in Call History

---
+++ app/src/main/kotlin/org/rivchain/cuplink/adapter/EventExpandableListAdapter.kt
@@ -0,0 +1,181 @@
+package org.rivchain.cuplink.adapter
+
+import android.content.Context
+import android.text.format.DateUtils
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseExpandableListAdapter
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.view.size
+import org.rivchain.cuplink.R
+import org.rivchain.cuplink.model.Contact
+import org.rivchain.cuplink.model.Event
+import org.rivchain.cuplink.util.Log
+import org.rivchain.cuplink.util.NetworkUtils
+import java.text.DateFormat
+import java.util.*
+import kotlin.math.abs
+
+internal class EventExpandableListAdapter(
+    private val context: Context,
+    private var events: List<Event>,
+    private var contacts: List<Contact>
+) : BaseExpandableListAdapter() {
+
+    var groupedEvents = groupAndCompactEvents(events)
+
+    fun update(events: List<Event>, contacts: List<Contact>) {
+        Log.d(this, "update() events=${events.size}, contacts=${contacts.size}")
+        this.groupedEvents = groupAndCompactEvents(events)
+        this.contacts = contacts
+        notifyDataSetChanged()
+    }
+
+    override fun getGroupCount(): Int = groupedEvents.size
+
+    override fun getChildrenCount(groupPosition: Int): Int = groupedEvents[groupPosition].second.size
+
+    override fun getGroup(groupPosition: Int): String = groupedEvents[groupPosition].first
+
+    override fun getChild(groupPosition: Int, childPosition: Int): List<Event> = groupedEvents[groupPosition].second[childPosition]
+
+    override fun getGroupId(groupPosition: Int): Long = groupPosition.toLong()
+
+    override fun getChildId(groupPosition: Int, childPosition: Int): Long = childPosition.toLong()
+
+    override fun hasStableIds(): Boolean = false
+
+    override fun getGroupView(groupPosition: Int, isExpanded: Boolean, convertView: View?, parent: ViewGroup): View {
+        val view = convertView ?: LayoutInflater.from(context).inflate(R.layout.item_event_group, parent, false)
+        val headerTv = view.findViewById<TextView>(R.id.call_header)
+        headerTv.text = getGroup(groupPosition)
+        return view
+    }
+
+    override fun getChildView(groupPosition: Int, childPosition: Int, isLastChild: Boolean, convertView: View?, parent: ViewGroup): View {
+        val view = convertView ?: LayoutInflater.from(context).inflate(R.layout.item_event, parent, false)
+        val event = getChild(groupPosition, childPosition)
+        // show list in reverse, latest element first
+        val latestEvent = event.last()
+
+        // find name
+        val name = contacts.find { it.publicKey.contentEquals(latestEvent.publicKey) }?.name
+
+        val nameTv = view.findViewById<TextView>(R.id.call_name)
+        if (name.isNullOrEmpty()) {
+            nameTv.text = view.context.getString(R.string.unknown_caller)
+        } else {
+            nameTv.text = name
+        }
+
+        // time how long ago the latest event happened
+        val dateTV = view.findViewById<TextView>(R.id.call_date)
+        val now = System.currentTimeMillis()
+        if (abs(now - latestEvent.date.time) < DateUtils.HOUR_IN_MILLIS) {
+            dateTV.text = DateUtils.getRelativeTimeSpanString(latestEvent.date.time, now, DateUtils.MINUTE_IN_MILLIS)
+        } else if (DateUtils.isToday(latestEvent.date.time)) {
+            val tf = DateFormat.getTimeInstance(DateFormat.SHORT)
+            dateTV.text = tf.format(latestEvent.date)
+        } else {
+            val df = DateFormat.getDateInstance(DateFormat.SHORT)
+            dateTV.text = df.format(latestEvent.date)
+        }
+
+        // add one icon for each event type add one extra for the last event
+        val iconsView = view.findViewById<LinearLayout>(R.id.call_icons)
+        val iconSet = mutableSetOf<Event.Type>()
+        iconsView.removeAllViews()
+        for (e in event) {
+            if (!iconSet.contains(e.type) || e === latestEvent) {
+                appendIcon(iconsView, e.type)
+                iconSet.add(e.type)
+            }
+        }
+
+        val addressTV = view.findViewById<TextView>(R.id.call_address)
+        val address = event.lastOrNull { it.address != null } ?.address
+
+        val addressString = NetworkUtils.inetSocketAddressToString(address)
+        if (addressString != null) {
+            addressTV.text = addressString
+        } else {
+            addressTV.text = ""
+        }
+
+        // show counter if not all calls have an icon
+        val eventCount = event.size
+        val iconCount = iconsView.size
+        val counterTV = view.findViewById<TextView>(R.id.call_counter)
+        if (eventCount > iconCount) {
+            counterTV.text = "(${eventCount})"
+        } else {
+            counterTV.text = ""
+        }
+
+        return view
+    }
+
+    override fun isChildSelectable(groupPosition: Int, childPosition: Int): Boolean = true
+
+    private fun appendIcon(iconsView: LinearLayout, eventType: Event.Type) {
+        val imageView = ImageView(iconsView.context)
+        imageView.setImageResource(
+            when (eventType) {
+                Event.Type.UNKNOWN -> R.drawable.ic_incoming_call_error
+                Event.Type.INCOMING_ACCEPTED -> R.drawable.ic_incoming_call_accepted
+                Event.Type.INCOMING_MISSED -> R.drawable.ic_incoming_call_missed
+                Event.Type.INCOMING_ERROR -> R.drawable.ic_incoming_call_error
+                Event.Type.OUTGOING_ACCEPTED -> R.drawable.ic_outgoing_call_accepted
+                Event.Type.OUTGOING_MISSED -> R.drawable.ic_outgoing_call_missed
+                Event.Type.OUTGOING_ERROR -> R.drawable.ic_outgoing_call_error
+            }
+        )
+        imageView.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        iconsView.addView(imageView)
+    }
+
+    companion object {
+
+        // group consecutive events of a single contact
+        fun groupAndCompactEvents(events: List<Event>): List<Pair<String, List<List<Event>>>> {
+            val groupedEvents = mutableMapOf(
+                "Today" to mutableListOf<List<Event>>(),
+                "Yesterday" to mutableListOf<List<Event>>(),
+                "Older" to mutableListOf<List<Event>>()
+            )
+            val now = System.currentTimeMillis()
+            val todayStart = now - (now % DateUtils.DAY_IN_MILLIS)
+            val yesterdayStart = todayStart - DateUtils.DAY_IN_MILLIS
+
+            var lastEvent: Event? = null
+            var lastEventList: MutableList<Event>? = null
+            var lastCategory: String? = null
+
+            for (event in events) {
+                val eventTime = event.date.time
+                val category = when {
+                    eventTime >= todayStart -> "Today"
+                    eventTime >= yesterdayStart -> "Yesterday"
+                    else -> "Older"
+                }
+
+                if (lastEvent == null || lastEventList == null || !lastEvent.publicKey.contentEquals(event.publicKey) || lastCategory != category) {
+                    lastEventList = mutableListOf(event)
+                    groupedEvents[category]?.add(lastEventList)
+                } else {
+                    lastEventList.add(event)
+                }
+                lastEvent = event
+                lastCategory = category
+            }
+
+            return groupedEvents.entries.map { it.key to it.value }
+        }
+    }
+}
